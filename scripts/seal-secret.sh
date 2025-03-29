40eb489e763909269896e5973d163d8a4e034976
Karol Szpakowski <karol.szpakowski@gmail.com>
2025-03-29 12:59:54 +0100

    add cert-manager application configuration and sealed secrets setup

---
+++ scripts/seal-secret.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+read -p "Secret name: " SECRET_NAME
+read -p "Secret namespace: " SECRET_NS
+read -p "Secret key: " SECRET_KEY
+read -p "Secret value: " SECRET_VALUE
+kubectl create secret generic $SECRET_NAME --from-literal=$SECRET_KEY='$SECRET_VALUE' --dry-run=client -o yaml | \
+  kubeseal -o yaml --scope namespace-wide -n $SECRET_NS | \
+  yq 'del(.metadata.creationTimestamp, .spec.template.metadata.creationTimestamp)' > \
+  $SECRET_NAME-sealedsecret.yaml
+
+echo "Sealed secret $SECRET_NAME-sealedsecret.yaml created."
